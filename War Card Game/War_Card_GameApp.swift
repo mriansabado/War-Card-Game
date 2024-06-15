f470ea6864d6d8e4a52c34c33cfc4414fe44efe9
Ian <isabado@onpointglobal.com>
2024-06-15 10:17:20 -0700

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Onpoint on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
